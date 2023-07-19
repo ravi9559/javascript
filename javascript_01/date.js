78fede3eda1c57000813e094215d5be0e1ed6fbe
Ravi Chaudhary <ravi9559@users.noreply.github.com>
2023-07-19 07:06:43 +0000

    string

---
+++ javascript_01/date.js
@@ -0,0 +1,20 @@
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log( typeof myDate);
+
+// let myCreateDate = new Date(2023, 0, 23)
+
+// let myCreateDate = new Date(2023, 0, 23, 5,3)
+
+// let myCreateDate = new Date("2023-01-14")
+
+
+console.log(myCreateDate.toLocaleString());
+
+
+let myCreateDate = new Date("2023-01-14")
+
+
