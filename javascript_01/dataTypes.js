a13f1309608bf5f2f6f6d55d2390fff48944cd31
Ravi Chaudhary <ravi9559@users.noreply.github.com>
2023-07-19 01:10:56 +0000

    dataTypes

---
+++ javascript_01/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"  //treat all js code as newer version
+
+// alert (3+3)  // we are using nodejs, not browser
+
+let name= "ravi";
+let age =18;
+let isLoggedIn= false;
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// standalone
+// null => standalone value
+// undefined =>
+//  symbole => unique
+
+// object 
+
+console.log(typeof undefined);
+
+
