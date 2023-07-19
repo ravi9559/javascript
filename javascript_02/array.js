64b58dfbfc488e7fdd0f194bce32f8f8926e6261
Ravi Chaudhary <ravi9559@users.noreply.github.com>
2023-07-19 07:25:25 +0000

    array

---
+++ javascript_02/array.js
@@ -0,0 +1,9 @@
+// const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+// console.log(shopping);
+
+const numbers = [5, 2, 7, 6];
+
+// console.log(numbers)
+// numbers.shift(7);
+
+console.log("A" , numbers);
