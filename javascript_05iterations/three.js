f19b8295d5a020139995a8ca1b9ea39e8622cb89
Ravi Chaudhary <ravi9559@users.noreply.github.com>
2023-08-01 05:56:19 +0000

    for loop

---
+++ javascript_05iterations/three.js
@@ -0,0 +1,28 @@
+// for of 
+
+// const arr = [1,2,3,4,5];
+
+// for (const  i  of arr){
+//     console.log(i);
+// }
+
+// const map = new Map()
+
+// map.set('IN', "India")
+// map.set('USA', "United state of America")
+// map.set('FR', "France")
+
+// //console.log(map)
+
+//  for (const [key, value] of map) {
+//     console.log(key, value)
+//  }
+
+const myObject ={
+    gmae1: 'NFS',
+    game2: 'Spiderman'
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, value)
+}
